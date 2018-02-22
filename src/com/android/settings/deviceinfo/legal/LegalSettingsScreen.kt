b5ffeca921021cdadc2c699b162543852c85cd91
Roman Birg <roman@cyngn.com>
2018-02-22 15:27:46 +0100

    Settings: Add LineageOS legal info
    Open up the LineageOS legal info in the browser.
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I263ccc0509e275d17512528deb606341d58e7a0d
    Ticket-Id: CYNGNOS-1895
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/deviceinfo/legal/LegalSettingsScreen.kt
+++ src/com/android/settings/deviceinfo/legal/LegalSettingsScreen.kt
@@ -47,6 +47,7 @@ open class LegalSettingsScreen : PreferenceScreenMixin {
         preferenceHierarchy(context) {
             +LegalPreference("copyright", R.string.copyright_title, "android.settings.COPYRIGHT")
             +LegalPreference("license", R.string.license_title, "android.settings.LICENSE")
+            +LineageLegalPreference("lineage_license", R.string.lineagelicense_title)
             +LegalPreference("terms", R.string.terms_title, "android.settings.TERMS")
             +ModuleLicensesScreen.KEY // Use screen key in case it is overlaid.
             +LegalPreference(
